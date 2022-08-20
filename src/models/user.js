06710bc1550cc87c2bc15422af5dfd5ec8a107d2
ranses1909 <canelonranses2@gmail.com>
2022-08-20 08:57:29 -0500

    main

--- src/models/user.js
+++ src/models/user.js
@@ -2,19 +2,32 @@ const mongoose = require('mongoose')
 const express = require('express')
 
 // Everything in Mongoose starts with a Schema. Each schema maps to a MongoDB collection and defines the shape of the documents within that collection.
-const userSchema = mongoose.Schema({
-    name:{type: String,
-        required: true,
-    },
-    profession: String,
-    age: {type: Number,
-    required: true
-    },
-    email:{
-    type: String,
-    required: true,
-    } 
-})
-
+// const userSchema = mongoose.Schema({
+//     name:{type: String,
+//         required: true,
+//     },
+//     profession: String,
+//     age: {type: Number,
+//     required: true
+//     },
+//     email:{
+//     type: String,
+//     required: true,
+//     } 
+// })
+const userSchema2 = mongoose.Schema({
+    result:{
+        description: {type: String,
+            required: true,
+        },
+        price: {type: String,
+            required: true
+        },
+        image:{
+            type: String,
+            required: true,
+        },}
+    
+},)
 // We need to call the model constructor on the Mongoose instance and pass it the name of the collection and a reference to the schema definition.
-module.exports = mongoose.model('user', userSchema)
+module.exports = mongoose.model('user', userSchema2)
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,7 @@
 
 // express tiene un constructor llamado router
 const express = require('express');
-const user = require('../models/user');
+// const user = require('../models/user');
 const userSchema = require('../models/user')
 const router = express.Router();
 // now we define routes
@@ -18,5 +18,47 @@ user
 })
 
 
+// get all users
+router.get('/users', (req, res) => {
+    // recibimos el objeto de la peticion que tiene un body
+userSchema
+// instead of saving we're gonna find all users
+.find()
+.then((data) => {res.json(data)})
+.catch((err) => res.json({message: err}))
+})
+
+// get one user
+router.get('/users/:id', (req, res) => {
+    // here we will fin the id we need, that id it's gonna be inside the params that comes from the petition
+    const { id } = req.params;
+    userSchema
+// instead of saving we're gonna find all users
+.findById(id)
+.then((data) => {res.json(data)})
+.catch((err) => res.json({message: err}))
+})
+
+// update one user
+router.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    // we pull up whose data we want to update from the body
+    const {name,age,email} = req.body;
+ userSchema
+//  this metod receive  2 params (2 objects) one with id and another with something like a set from useState that needs ($set)
+ .updateOne({_id:id}, {$set: {name,age,email}})
+.then((data) => {res.json(data)})
+.catch((err) => res.json({message: err}))
+})
     
+router.delete('/users/:id', (req, res) =>{
+    const { id } = req.params;
+userSchema
+.remove({_id:id})
+.then((data) => {res.json(data)})
+.catch((err) => res.json({message: err}))
+})
 module.exports = router;
+
+// api key
+// cnrjiJT6duQrQXIsX9T4VGjDLBCvN7xSHduEx62TarBhlPLUKYOlq9sBQSaxTnbe
